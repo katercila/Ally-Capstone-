f7b51ffe9611660849d95de8210fcf8a3a54c4b0
katercila <119532755+katercila@users.noreply.github.com>
2023-04-19 11:18:10 -0400

    created links

---
+++ src/components/SignUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "../styling/Home.css";
+
+
+function SignUp() {
+  return (
+    <main>
+      <div className="intro">
+        <h1>Sign Up Page</h1>
+      </div>
+    </main>
+  );
+}
+
+export default SignUp;
